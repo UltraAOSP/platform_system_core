3acc1c59c64c417e9da1405325b6afcd94f9a039
Ahmed Hady <ahmedhady6@gmail.com>
2017-11-22 21:51:03 +0200

    debuggerd: add AOST version to tombstones

--- debuggerd/tombstone.cpp
+++ debuggerd/tombstone.cpp
@@ -171,10 +171,13 @@ static const char* get_sigcode(int signo, int code) {
 static void dump_header_info(log_t* log) {
   char fingerprint[PROPERTY_VALUE_MAX];
   char revision[PROPERTY_VALUE_MAX];
+  char aost_version[PROPERTY_VALUE_MAX];
 
+  property_get("ro.aost.version", aost_version, "unknown");
   property_get("ro.build.fingerprint", fingerprint, "unknown");
   property_get("ro.revision", revision, "unknown");
 
+  _LOG(log, logtype::HEADER, "AOST Version: '%s'\n", aost_version);
   _LOG(log, logtype::HEADER, "Build fingerprint: '%s'\n", fingerprint);
   _LOG(log, logtype::HEADER, "Revision: '%s'\n", revision);
   _LOG(log, logtype::HEADER, "ABI: '%s'\n", ABI_STRING);
